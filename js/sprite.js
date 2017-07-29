5fe9ebbed5df7dc5b7b4d460ce3d09714869aaef
YoPox <yopoxdev+github@gmail.com>
2017-07-29 16:39:25 +0200

    Arcade Physics…

--- js/sprite.js
+++ js/sprite.js
@@ -1,16 +1,6 @@
 // Fonctions utiles sur les sprites
 
-function initSprite(sprite, anchor, scale) {
-  sprite.smoothed = false;
-  sprite.anchor.x = anchor[0];
-  sprite.anchor.y = anchor[1];
-  sprite.scale.x = scale[0];
-  sprite.scale.y = scale[1];
-}
-
-function initLayer(layer) {
-  layer.smoothed = false;
-  layer.scale.x = 2;
-  layer.scale.y = 2;
-  layer.resizeWorld();
+function initSprite(sprite, anchor) {
+    sprite.anchor.x = anchor[0];
+    sprite.anchor.y = anchor[1];
 }
--- js/states/play.js
+++ js/states/play.js
@@ -1,51 +1,59 @@
 var rob;
 var justPressedSpace = false;
-var map, layerGround, layerItems;
+var map;
+var layerGround;
+var layerItems;
 
 var playState = {
-  create: function() {
+    create: function() {
 
-    // MUSIC PLAYBACK
-    // var buffer = game.cache.getBinary('xm');
-    // ArtRemix.play(buffer);
+        // MUSIC PLAYBACK
+        // var buffer = game.cache.getBinary('xm');
+        // ArtRemix.play(buffer);
 
-    game.stage.backgroundColor = "#EEEEEE";
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = "#EEEEEE";
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    map = game.add.tilemap('map1', 16, 16);
-    map.addTilesetImage('tiles');
-    layerGround = map.createLayer('ground');
-    initLayer(layerGround);
-    layerItems = map.createLayer('item1');
-    initLayer(layerItems);
+        // Map init
+        map = game.add.tilemap('map1');
+        map.addTilesetImage('tiles');
+        map.setCollisionBetween(0, 100, true);
+        layerGround = map.createLayer('ground');
+        game.physics.arcade.enable(layerGround);
+        layerGround.resizeWorld();
+        // initLayer(layerGround);
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    map.setCollisionBetween(0, 100, true, 0);
 
-    // rob = game.add.sprite(64, 576 - 128, 'robot');
-    rob = game.add.sprite(64, 500 - 128, 'robot');
-    rob.animations.add('up', [0, 1, 2, 3, 4]);
-    initSprite(rob, [0, 1], [4, 4]);
-    game.physics.arcade.enable(rob);
-    rob.body.velocity.x = 250;
-    rob.body.gravity.y = 400;
-    rob.body.bounce.y = 0.5;
-    rob.body.collideWorldBounds = true;
-    game.camera.follow(rob);
+        // layerItems = map.createLayer('item1');
+        // initLayer(layerItems);
 
-    game.renderer.renderSession.roundPixels = true;
+        rob = game.add.sprite(64, 288 - 136, 'robot');
+        initSprite(rob, [0, 0]);
+        game.physics.arcade.enable(rob);
+        rob.body.setSize(10, 32, 3, 32);
+        rob.body.velocity.x = 125;
+        rob.body.gravity.y = 200;
+        rob.body.bounce.y = 0.2;
+        rob.body.collideWorldBounds = true;
+        game.camera.follow(rob);
 
-  },
+        game.renderer.renderSession.roundPixels = true;
 
-  update: function() {
-    game.physics.arcade.collide(rob, layerGround);
-    if (spaceKey.isDown) {
-      if (!justPressedSpace) {
-        rob.animations.play('up', 20, 0);
-        justPressedSpace = true;
-      }
-    } else {
-      justPressedSpace = false;
+    },
+
+    update: function() {
+
+        game.physics.arcade.collide(rob, layerGround);
+
+    },
+
+    render: function() {
+        game.debug.body(rob);
     }
-  }
+
 };
+
+function collect1(sprite, tile) {
+    tile.alpha = 0;
+    console.log("test");
+}
