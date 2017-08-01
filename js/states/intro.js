a1470718cad47d5fdce8980a7d51d2aa22c46c66
YoPox <yopoxdev+github@gmail.com>
2017-08-01 02:44:41 +0200

    Intro

--- js/states/intro.js
+++ js/states/intro.js
@@ -1,42 +1,54 @@
-var text1 = "In the near future, Rob is a robot in a nuclear plant.\nHe has to bring to the reactor uranium to power it.";
-var text2 = "But one day, the nuclear plant explosed, destroying every life on the planet, except Rob.";
-var text3 = "To keep ";
+var text1 = "In year 2064, a robot works in a nuclear plant.";
+var text2 = "But one day, the nuclear plant explosed,\ndestroying every life on the planet\nbut the robot.";
+var text3 = "He must collect uranium to power his radio,\nhis only hope of finding someone.";
 
 
 var introState = {
-  create: function() {
-    console.log('Intro');
+    create: function() {
 
-    game.stage.backgroundColor = "#000000";
+        game.stage.backgroundColor = "#000000";
 
-    bitmapText1 = game.add.bitmapText(game.width / 2, game.height * 0.1 + 16, "SullyVerge", text1, 16);
-    initSprite(bitmapText1, [0.5, 0]);
-    bitmapText1.alpha = 0;
-    game.add.tween(bitmapText1).to({
-      y: bitmapText1.y - 16,
-      alpha: 1
-    }, 1000, Phaser.Easing.Cubic.InOut, true, 1000);
+        bitmapText1 = game.add.bitmapText(game.width / 2, game.height / 2 - 64, "SullyVerge", text1, 16);
+        initSprite(bitmapText1, [0.5, 0.5]);
+        bitmapText1.alpha = 0;
+        game.add.tween(bitmapText1).to({
+            y: bitmapText1.y - 16,
+            alpha: 1
+        }, 1000, Phaser.Easing.Cubic.InOut, true, 1000);
 
-    bitmapText2 = game.add.bitmapText(game.width / 2, game.height * 0.4 + 16, "SullyVerge", text2, 16);
-    initSprite(bitmapText2, [0.5, 0]);
-    bitmapText2.alpha = 0;
-    game.add.tween(bitmapText2).to({
-      y: bitmapText2.y - 16,
-      alpha: 1
-    }, 1000, Phaser.Easing.Cubic.InOut, true, 3000);
+        bitmapText2 = game.add.bitmapText(game.width / 2, game.height / 2, "SullyVerge", text2, 16);
+        bitmapText2.align = "center";
+        initSprite(bitmapText2, [0.5, 0.5]);
+        bitmapText2.alpha = 0;
+        game.add.tween(bitmapText2).to({
+            y: bitmapText2.y - 16,
+            alpha: 1
+        }, 1000, Phaser.Easing.Cubic.InOut, true, 3000);
 
-    bitmapText3 = game.add.bitmapText(game.width / 2, game.height * 0.7 + 16, "SullyVerge", text3, 16);
-    initSprite(bitmapText3, [0.5, 0]);
-    bitmapText3.alpha = 0;
-    game.add.tween(bitmapText3).to({
-      y: bitmapText3.y - 16,
-      alpha: 1
-    }, 1000, Phaser.Easing.Cubic.InOut, true, 5000);
-  },
+        bitmapText3 = game.add.bitmapText(game.width / 2, game.height / 2 + 64, "SullyVerge", text3, 16);
+        bitmapText3.align = "center";
+        initSprite(bitmapText3, [0.5, 0.5]);
+        bitmapText3.alpha = 0;
+        game.add.tween(bitmapText3).to({
+            y: bitmapText3.y - 16,
+            alpha: 1
+        }, 1000, Phaser.Easing.Cubic.InOut, true, 5000);
+    },
 
-  update: function() {
-    if (spaceKey.isDown) {
-      game.state.start('menu');
+    update: function() {
+        if (spaceKey.isDown) {
+            var tween = game.add.tween(bitmapText1).to({
+                alpha: 0
+            }, 1000, Phaser.Easing.Cubic.InOut, true);
+            game.add.tween(bitmapText2).to({
+                alpha: 0
+            }, 1000, Phaser.Easing.Cubic.InOut, true);
+            game.add.tween(bitmapText3).to({
+                alpha: 0
+            }, 1000, Phaser.Easing.Cubic.InOut, true);
+            tween.onComplete.add(function() {
+                game.state.start('menu');
+            })
+        }
     }
-  }
 }
