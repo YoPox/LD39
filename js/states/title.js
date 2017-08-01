30c397dc53daef34405d7537884f9b50d1ffb11f
Louis Duret-Robert <louisduret@gmail.com>
2017-08-01 02:08:20 +0200

    Merge branch 'master' of https://github.com/YoPox/LD39

--- js/states/title.js
+++ js/states/title.js
@@ -1,5 +1,6 @@
 var keyDown = false;
-var titleText = [];
+var titleImage;
+var titleText;
 var titleInterval1;
 var titleInterval2;
 var titleJump = false;
@@ -24,11 +25,24 @@ var titleState = {
         map = game.add.tilemap('titleMap');
         map.addTilesetImage('tiles');
         map.setCollisionBetween(0, 100, true);
+
         // Layers
         layerGround = map.createLayer('ground');
         game.physics.arcade.enable(layerGround);
         layerGround.resizeWorld();
         layerScenery = map.createLayer('scenery');
+
+        // Moving things
+        deco = [game.add.group(), game.add.group(), game.add.group()];
+        map.createFromObjects('objects', 60, 'fly', 0, true, false, deco[0]);
+        deco[0].callAll('animations.add', 'animations', 'idle', [0, 0, 1, 2, 2, 2, 3, 4, 5, 5, 4, 3, 2, 2, 1], 6, true);
+        deco[0].callAll('animations.play', 'animations', 'idle');
+        map.createFromObjects('objects', 61, 'mouse', 0, true, false, deco[1]);
+        deco[1].callAll('animations.add', 'animations', 'idle', [0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8], 6, true);
+        deco[1].callAll('animations.play', 'animations', 'idle');
+        map.createFromObjects('objects', 62, 'spider', 0, true, false, deco[2]);
+        deco[2].callAll('animations.add', 'animations', 'idle', [0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 2, 1], 6, true);
+        deco[2].callAll('animations.play', 'animations', 'idle');
 
         // Steam
         steam = game.add.group();
@@ -52,10 +66,10 @@ var titleState = {
         crouched = false;
 
         // Text
-        titleText.push(game.add.bitmapText(game.width / 2, game.height / 4, 'SullyVerge', 'Energy Runner', 64));
-        initSprite(titleText[0], [0.5, 0.5]);
-        titleText.push(game.add.bitmapText(game.width / 2, game.height / 2 + 16, 'SullyVerge', 'Press SPACE to start', 16));
-        initSprite(titleText[1], [0.5, 0.5]);
+        titleImage = game.add.sprite(game.width / 2, game.height / 4, 'urunium');
+        initSprite(titleImage, [0.5, 0.5]);
+        titleText = game.add.bitmapText(game.width / 2, game.height / 2 + 16, 'SullyVerge', 'Press SPACE to start', 16);
+        initSprite(titleText, [0.5, 0.5]);
 
         // Rob animation
         // Crouch
@@ -88,7 +102,7 @@ var titleState = {
             } else if (rob.y == 208) {
                 titleJump = false;
             }
-            titleText[1].alpha = 1 - titleText[1].alpha;
+            titleText.alpha = 1 - titleText.alpha;
         }, 500);
 
         graphics = game.add.graphics(0, 0);
@@ -102,13 +116,12 @@ var titleState = {
         if (spaceKey.isDown && !pressedSpace) {
             pressedSpace = true;
             sfx[7].play();
-            cleanTitle();
             transition.active = true;
             game.add.tween(transition).to({
                 radius: 0
             }, 800, Phaser.Easing.Cubic.In, true);
             setTimeout(function() {
-                graphics.kill();
+                cleanTitle();
                 game.state.start("menu");
             }, 1300);
         }
@@ -132,5 +145,12 @@ var titleState = {
 function cleanTitle() {
     clearInterval(titleInterval1);
     clearInterval(titleInterval2);
+<<<<<<< HEAD
     titleText = [];
 }
+=======
+    titleText.kill();
+    titleImage.kill();
+    graphics.kill();
+}
+>>>>>>> 9b2d51d74e907976efce94db762a62c47d7b43d9
